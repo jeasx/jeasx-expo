abaca335ce08ade9496f582d82f05b889eae4d4d
Maik Jablonski <jablonski@neoskop.de>
2024-01-29 10:25:08 +0100

    dep. updates

--- src/routes/api/[...path].js
+++ src/routes/api/[...path].js
@@ -5,6 +5,7 @@ export default function Status({ request, reply }) {
   const { url, query, method, headers, body } = request;
   reply.header("Content-Type", "application/json");
   return {
+    node: `${process.version} (${process.arch})`,
     url,
     query,
     method,
