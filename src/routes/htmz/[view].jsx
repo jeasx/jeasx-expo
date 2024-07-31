063a65781af8ddca28d71448c7345c06f7def637
Maik Jablonski <jablonski@neoskop.de>
2024-07-31 11:28:20 +0200

    added htmz example

---
+++ src/routes/htmz/[view].jsx
@@ -0,0 +1,21 @@
+import fields from "./fields";
+
+/**
+ * @param {import("../types").RouteProps} props
+ */
+export default function View({ request, reply }) {
+  const body = request.body || {};
+
+  return (
+    <form id="form" action="./edit#form" target="htmz" method="post">
+      <input type="hidden" name="action" value="edit" />
+      {fields.map(({ label, name }) => (
+        <div>
+          <label>{label}:</label> {body[name]}
+          <input type="hidden" name={name} value={body[name]} />
+        </div>
+      ))}
+      <button>Edit</button>
+    </form>
+  );
+}
