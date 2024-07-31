8e7721904aa750992da73370f0532c1f15d685de
Maik Jablonski <jablonski@neoskop.de>
2024-07-31 11:38:01 +0200

    Added sha-256 for htmz

--- src/routes/Layout.jsx
+++ src/routes/Layout.jsx
@@ -15,12 +15,10 @@ export default function Layout({
       <html lang="en">
         <head>
           <base href={`${path.endsWith("/") ? path : path + "/"}`} />
-          {process.env.NODE_ENV !== "development" && (
-            <meta
-              http-equiv="Content-Security-Policy"
-              content="default-src 'none'; script-src 'self' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; font-src 'self'; img-src 'self' data:; object-src 'none'; form-action 'self'; base-uri 'self'; connect-src 'self'; frame-src 'self';"
-            />
-          )}
+          <meta
+            http-equiv="Content-Security-Policy"
+            content="default-src 'none'; script-src 'self' 'unsafe-eval' 'unsafe-hashes' 'sha256-RtgCm65gfpyTapAUQ8oRVHqX1x3cADleYOksfq8Me/Y='; style-src 'self' 'unsafe-inline'; font-src 'self'; img-src 'self' data:; object-src 'none'; form-action 'self'; base-uri 'self'; connect-src 'self'; frame-src 'self';"
+          />
           <meta http-equiv="Referrer-Policy" content="same-origin" />
           <meta charset="utf-8" />
           <meta name="description" content={description} />
