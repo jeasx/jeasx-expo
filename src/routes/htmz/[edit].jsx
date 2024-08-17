f0b9d550ca52bd839565aa1341c5fa7580ad623a
Maik Jablonski <jablonski@neoskop.de>
2024-08-17 20:44:32 +0200

    Revert "removed htmz example"
    This reverts commit cfe17082fcef3c72ffae516ae98b91ededd42d0d.

---
+++ src/routes/htmz/[edit].jsx
@@ -0,0 +1,21 @@
+import fields from "./fields";
+
+/**
+ * @param {import("../types").RouteProps} props
+ */
+export default function Form({ request, reply }) {
+  const body = request.body || {};
+
+  return (
+    <form id="swap" action="./view#swap" target="htmz" method="post">
+      <input type="hidden" name="action" value="view" />
+      {fields.map(({ label, name, type, value }) => (
+        <div>
+          <label>{label}:</label>
+          <input type={type} name={name} value={body[name] ?? value} />
+        </div>
+      ))}
+      <button>Submit</button>
+    </form>
+  );
+}
