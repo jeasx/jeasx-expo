65325faae268debcab9fcbc8aab7d72ea7437284
Maik Jablonski <jablonski@neoskop.de>
2024-10-31 12:12:50 +0100

    Added example for template fragments

---
+++ src/routes/fragments/[...path].jsx
@@ -0,0 +1,77 @@
+import Layout from "../Layout";
+
+export default async function Fragments({ request }) {
+  const id = request.query["id"] || 1;
+  const product = await (
+    await fetch(`https://dummyjson.com/products/${id}`)
+  ).json();
+
+  const OptionalLayout = ({ children = [] }) =>
+    request.path.includes("~") ? (
+      <>{children}</>
+    ) : (
+      <Layout script="/htmx/index.js">{children}</Layout>
+    );
+
+  return (
+    <OptionalLayout>
+      <Fragment>
+        <h1>Template Fragments</h1>
+        <p>
+          Creating routes that emit HTML fragments is straightforward in Jeasx
+          which makes it a perfect match for{" "}
+          <a href="https://htmx.org/" target="_blank">
+            HTMX
+          </a>
+          . Sometimes, however, creating many routes by hand can be tedious. The
+          concept of{" "}
+          <a href="https://htmx.org/essays/template-fragments/" target="_blank">
+            template fragments
+          </a>{" "}
+          is a nice idea and can be easily implemented in Jeasx using just one
+          line of custom code.
+        </p>
+      </Fragment>
+      <Fragment name="product">
+        <form>
+          <h2>Product-Browser</h2>
+          <input
+            name="id"
+            type="range"
+            value={id}
+            min={1}
+            max={20}
+            hx-get="./~product"
+            hx-swap="outerHTML"
+            hx-target="closest form"
+          />
+          <article>
+            <header>
+              <h3>{product.title}</h3>
+              <b>{product.price} €</b>
+            </header>
+            <img
+              src={product.thumbnail}
+              alt={product.title}
+              width="300"
+              height="300"
+            />
+            <p>{product.description}</p>
+          </article>
+        </form>
+      </Fragment>
+      <Fragment name="time">
+        <p hx-get="./~time" hx-trigger="every 1s" hx-swap="outerHTML">
+          {new Date().toTimeString()}
+        </p>
+      </Fragment>
+    </OptionalLayout>
+  );
+}
+
+function Fragment({ name = "", children = [] }) {
+  return (
+    (!this.request.path.includes("~") ||
+      (name && this.request.path.endsWith(`~${name}`))) && <>{children}</>
+  );
+}
