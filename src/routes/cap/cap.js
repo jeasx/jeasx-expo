e275049061576d067095cc325298b677ce161f33
Maik Jablonski <jablonski@neoskop.de>
2025-08-06 16:11:59 +0200

    updated to new cap storage api

--- src/routes/cap/cap.js
+++ src/routes/cap/cap.js
@@ -1,8 +1,50 @@
 import Cap from "@cap.js/server";
 
 if (!globalThis.cap) {
+  const cache = { tokens: new Map(), challenges: new Map() };
   globalThis.cap = new Cap({
-    tokens_store_path: ".tokens.json",
+    storage: {
+      challenges: {
+        store: async (token, challengeData) => {
+          cache.challenges.set(token, challengeData);
+        },
+        read: async (token) => {
+          const data = cache.challenges.get(token);
+          return data?.expires > Date.now() ? data : null;
+        },
+        delete: async (token) => {
+          cache.challenges.delete(token);
+        },
+        listExpired: async () => {
+          const now = Date.now();
+          return cache.challenges
+            .entries()
+            .filter(([, { expires }]) => expires < now)
+            .map(([token]) => token)
+            .toArray();
+        },
+      },
+      tokens: {
+        store: async (key, expires) => {
+          cache.tokens.set(key, expires);
+        },
+        get: async (key) => {
+          const expires = cache.tokens.get(key);
+          return expires > Date.now() ? expires : null;
+        },
+        delete: async (key) => {
+          cache.tokens.delete(key);
+        },
+        listExpired: async () => {
+          const now = Date.now();
+          return cache.tokens
+            .entries()
+            .filter(([, expires]) => expires < now)
+            .map(([token]) => token)
+            .toArray();
+        },
+      },
+    },
   });
 }
 
