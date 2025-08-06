51922ada1df7f672ee88fdf070249acf2d1fa4fe
Maik Jablonski <jablonski@neoskop.de>
2025-08-06 15:52:28 +0200

    "@cap.js/server": "3.0.0"

--- src/routes/cap/api/[challenge].js
+++ src/routes/cap/api/[challenge].js
@@ -3,7 +3,7 @@ import cap from "../cap.js";
 /**
  * @param {import("../../types").RouteProps} props
  */
-export default function ({ request, reply }) {
+export default async function ({ request, reply }) {
   reply.type("application/json");
-  reply.send(cap.createChallenge());
+  reply.send(await cap.createChallenge());
 }
