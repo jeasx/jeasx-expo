13feac8fa17603f140c6b6e04e055650b9f8c2e0
Maik Jablonski <jablonski@neoskop.de>
2025-12-04 16:53:42 +0100

    prepare asset gzip

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:lts-alpine
 
-RUN apk add --no-cache curl bash 
+RUN apk add --no-cache gzip curl bash 
 
 USER node
 WORKDIR /home/node
@@ -13,4 +13,6 @@ RUN curl -fsSL https://bun.sh/install | bash
 ENV PATH="/home/node/.bun/bin:$PATH"
 
 RUN node --run build
+RUN gzip -rk public dist/browser
+
 ENTRYPOINT ["/home/node/entrypoint.sh"]
--- src/[...guard].js
+++ src/[...guard].js
@@ -1,3 +1,6 @@
+import { promisify } from "node:util";
+import { gzip } from "node:zlib";
+
 /**
  * @param {import("./types").RouteProps} props
  */
@@ -5,4 +8,16 @@ export default function ({ request, reply }) {
   // Prepare "this" context
   this.request = request;
   this.reply = reply;
+
+  this.responseHandler = (payload) => {
+    if (
+      typeof payload === "string" &&
+      request.headers["accept-encoding"]?.includes("gzip")
+    ) {
+      reply.header("content-encoding", "gzip");
+      return promisify(gzip)(payload);
+    } else {
+      return payload;
+    }
+  };
 }
