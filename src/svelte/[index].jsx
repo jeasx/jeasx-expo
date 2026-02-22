8b0296fc00d186ab46f34ab8821e829487a56b50
Maik Jablonski <jablonski@neoskop.de>
2026-02-22 17:28:34 +0100

    link to svelte.dev

--- src/svelte/[index].jsx
+++ src/svelte/[index].jsx
@@ -9,7 +9,10 @@ export default function () {
         <h1>Svelte example</h1>
         <section>
           <p>
-            Svelte is a UI framework that uses a compiler to let you write breathtakingly concise
+            <a href="https://svelte.dev" target="_blank">
+              Svelte
+            </a>{" "}
+            is a UI framework that uses a compiler to let you write breathtakingly concise
             components that do minimal work in the browser, using languages you already know — HTML,
             CSS and JavaScript. It’s a love letter to web development.
           </p>
