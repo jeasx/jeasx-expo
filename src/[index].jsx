fad18f6c795f3fe3c36c91967ee43faf5dd4d6a8
Maik Jablonski <jablonski@neoskop.de>
2026-03-19 18:58:34 +0100

    cleanups

--- src/[index].jsx
+++ src/[index].jsx
@@ -4,48 +4,43 @@ import sitemap from "./sitemap.json";
 export default function () {
   return (
     <Layout title="The power of Jeasx in examples" description="Welcome to Jeasx - JSX with Ease">
-      <section class="hero hero-primary">
-        <div class="container">
-          <h1>Explore Jeasx</h1>
-          <p class="hero-tagline">
-            Asynchronous JSX as self-evident templating technology
-            <br />
-            for the server-side rendered web.
-          </p>
-          <p>
-            Visit <a href="https://www.jeasx.dev">jeasx.dev</a> to learn more about the framework...
-          </p>
-        </div>
+      <section class="container hero hero-primary">
+        <h1>Explore Jeasx</h1>
+        <p class="hero-tagline">
+          Use asynchronous JSX as self-evident templating technology
+          <br />
+          for the server-side rendered web.
+        </p>
+        <p>
+          Visit <a href="https://www.jeasx.dev">jeasx.dev</a> to learn more about the framework.
+        </p>
       </section>
-      <section>
-        <div class="container center">
-          <p>
-            The examples are styled with <a href="https://mucss.org/">µCSS</a> (a lightweight,
-            themeable CSS framework), so they are easier to read in source code. The code is also
-            optimized for readability: no unnecessary abstractions, no fancy stuff, just simple and
-            clean code to get you started quickly.
-          </p>
-          <p>
-            You can find the complete repository on{" "}
-            <a href="https://github.com/jeasx/jeasx-expo">GitHub</a> to start experimenting with
-            Jeasx right on your computer.
-          </p>
-        </div>
+      <section class="container center">
+        <p>
+          You can find the complete repository on{" "}
+          <a href="https://github.com/jeasx/jeasx-expo">GitHub</a> to start experimenting with Jeasx
+          right on your computer.
+        </p>
+        <p>
+          The examples are styled with <a href="https://mucss.org/">µCSS</a> (a lightweight, mostly
+          classless CSS framework), so they are easier to read in source code.
+        </p>
       </section>
-      {sitemap.map(({ title, description, links }) => (
-        <section class="center">
-          <hr />
-          <h2>{title}</h2>
-          <p>{description}</p>
-          <div class="grid">
-            {links.map(({ title, path }) => (
-              <a href={path}>
-                <article>{title}</article>
-              </a>
-            ))}
-          </div>
-        </section>
-      ))}
+      <section class="container center">
+        {sitemap.map(({ title, description, links }) => (
+          <article class="card-secondary">
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <div class="row center">
+              {links.map(({ title, path }) => (
+                <a href={path} class="col-sm-6 col-md-4 col-lg-3">
+                  <article class="card-primary">{title}</article>
+                </a>
+              ))}
+            </div>
+          </article>
+        ))}
+      </section>
     </Layout>
   );
 }
