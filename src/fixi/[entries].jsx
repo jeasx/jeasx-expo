5b2794bdf4f30aebe3b25672ec6e7d75ecd9fda9
Maik Jablonski <jablonski@neoskop.de>
2026-03-21 12:00:41 +0100

    added fixi.js example

---
+++ src/fixi/[entries].jsx
@@ -0,0 +1,66 @@
+const entries = (globalThis.entries = {
+  0: { name: "Jeasx", link: "https://www.jeasx.dev" },
+  1: { name: "jsx-async-runtime", link: "https://github.com/jeasx/jsx-async-runtime" },
+  2: { name: "Fastify", link: "https://fastify.dev" },
+  3: { name: "esbuild", link: "https://esbuild.github.io" },
+});
+
+/**
+ * @param {import("../types").RouteProps} props
+ */
+export default function ({ request }) {
+  const body = request?.body || {};
+
+  if (["save", "create"].includes(body["action"])) {
+    entries[body["id"]] = {
+      name: body["name"],
+      link: body["link"],
+    };
+  }
+
+  return (
+    <article id="entries" class="card-secondary">
+      {Object.entries(entries).map(([id, { name, link }]) => {
+        const isEditMode = body["id"] === id && ["edit", "create"].includes(body["action"]);
+        return (
+          <form class="row" fx-action="./entries" fx-method="post" fx-target="#entries">
+            <input type="hidden" name="id" value={id} />
+            <div class="col-md-4 col-sm-12">
+              {isEditMode ? (
+                <input type="text" name="name" value={name} placeholder="Please enter the name" />
+              ) : (
+                name
+              )}
+            </div>
+            <div class="col-md-6 col-sm-12">
+              {isEditMode ? (
+                <input type="text" name="link" value={link} placeholder="Please enter the URL" />
+              ) : (
+                link
+              )}
+            </div>
+            <div class="col-md-2 col-sm-12">
+              {isEditMode ? (
+                <button type="submit" name="action" value="save">
+                  Save
+                </button>
+              ) : (
+                <button type="submit" name="action" value="edit">
+                  Edit
+                </button>
+              )}
+            </div>
+          </form>
+        );
+      })}
+      <form class="row" fx-action="./entries" fx-method="post" fx-target="#entries">
+        <div class="offset-md-10 col-md-2 col-sm-12">
+          <input type="hidden" name="id" value={Object.keys(entries).length} />
+          <button type="submit" name="action" value="create">
+            Add
+          </button>
+        </div>
+      </form>
+    </article>
+  );
+}
