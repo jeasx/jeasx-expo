793db27f56d73ff4bc6fcce45f16358f13e8917b
Maik Jablonski <jablonski@neoskop.de>
2026-03-21 16:11:19 +0100

    fixi example

--- src/fixi/[index].jsx
+++ src/fixi/[index].jsx
@@ -20,20 +20,17 @@ export default function ({ request }) {
           Fixi.js
         </a>{" "}
         is a minimalist alternative to HTMX, developed by the creators of HTMX. Enhancing a Jeasx
-        server rendered application with Fixi.js works without problems.
+        server rendered application with Fixi.js works without problems. The example illustrates
+        progressive enhancement: it provides an improved user experience with JavaScript while
+        remaining fully functional as a fallback without it.
       </p>
-      <h2>Link editor example</h2>
+      <h2>Recipe editor</h2>
       <Fragment request={request} />
     </Layout>
   );
 }
 
-const entries = (globalThis.entries = {
-  0: { name: "Jeasx", link: "https://www.jeasx.dev" },
-  1: { name: "jsx-async-runtime", link: "https://github.com/jeasx/jsx-async-runtime" },
-  2: { name: "Fastify", link: "https://fastify.dev" },
-  3: { name: "esbuild", link: "https://esbuild.github.io" },
-});
+const entries = (await (await fetch(`https://dummyjson.com/recipes`)).json()).recipes.slice(0, 5);
 
 function Fragment({ request }) {
   const body = request?.body || {};
@@ -41,32 +38,52 @@ function Fragment({ request }) {
   if (["save", "create"].includes(body["action"])) {
     entries[body["id"]] = {
       name: body["name"],
-      link: body["link"],
+      cuisine: body["cuisine"],
+      difficulty: body["difficulty"],
     };
   }
 
   return (
     <article id="entries" class="card-secondary">
-      {Object.entries(entries).map(([id, { name, link }]) => {
+      {Object.entries(entries).map(([id, { name, cuisine, difficulty }]) => {
         const isEditMode = body["id"] === id && ["edit", "create"].includes(body["action"]);
         return (
-          <form class="row" fx-action="." fx-method="post" fx-target="#entries">
+          <form
+            class="row"
+            action="."
+            method="post"
+            fx-action="."
+            fx-method="post"
+            fx-target="#entries"
+          >
             <input type="hidden" name="id" value={id} />
-            <div class="col-md-4 col-sm-12">
+            <div class="col-md-3 col-sm-12">
               {isEditMode ? (
-                <input type="text" name="name" value={name} placeholder="Please enter the name" />
+                <input type="text" name="name" value={name} placeholder="Name..." />
               ) : (
                 name
               )}
             </div>
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-3 col-sm-12">
               {isEditMode ? (
-                <input type="text" name="link" value={link} placeholder="Please enter the URL" />
+                <input type="text" name="cuisine" value={cuisine} placeholder="Cuisine..." />
               ) : (
-                link
+                cuisine
               )}
             </div>
-            <div class="col-md-2 col-sm-12">
+            <div class="col-md-3 col-sm-12">
+              {isEditMode ? (
+                <input
+                  type="text"
+                  name="difficulty"
+                  value={difficulty}
+                  placeholder="Difficulty..."
+                />
+              ) : (
+                difficulty
+              )}
+            </div>
+            <div class="col-md-3 col-sm-12">
               {isEditMode ? (
                 <button type="submit" name="action" value="save">
                   Save
@@ -80,9 +97,16 @@ function Fragment({ request }) {
           </form>
         );
       })}
-      <form class="row" fx-action="." fx-method="post" fx-target="#entries">
-        <div class="offset-md-10 col-md-2 col-sm-12">
-          <input type="hidden" name="id" value={Object.keys(entries).length} />
+      <form
+        class="row"
+        action="."
+        method="post"
+        fx-action="."
+        fx-method="post"
+        fx-target="#entries"
+      >
+        <div class="offset-md-9 col-md-3 col-sm-12">
+          <input type="hidden" name="id" value={entries.length} />
           <button type="submit" name="action" value="create">
             Add
           </button>
