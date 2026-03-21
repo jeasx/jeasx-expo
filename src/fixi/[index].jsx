c6bddd9802047f7d5fa92cf125243a655d8c077e
Maik Jablonski <jablonski@neoskop.de>
2026-03-21 12:22:12 +0100

    simplified fixi example

--- src/fixi/[index].jsx
+++ src/fixi/[index].jsx
@@ -1,3 +1,33 @@
+import Layout from "../Layout";
+
+/**
+ * @param {import("../types").RouteProps} props
+ */
+export default function ({ request }) {
+  if (request.headers["fx-request"] === "true") {
+    return <Fragment request={request} />;
+  }
+
+  return (
+    <Layout
+      title="Fixi.js and Jeasx - lightweight hypermedia controls"
+      description="Jeasx integrates easily with fixi.js"
+      script="./index.js"
+    >
+      <h1>Fixi.js and Jeasx - lightweight hypermedia controls</h1>
+      <p>
+        <a href="https://github.com/bigskysoftware/fixi" target="_blank">
+          Fixi.js
+        </a>{" "}
+        is a minimalist alternative to HTMX, developed by the creators of HTMX. Enhancing a Jeasx
+        server rendered application with Fixi.js works without problems.
+      </p>
+      <h2>Link editor example</h2>
+      <Fragment request={request} />
+    </Layout>
+  );
+}
+
 const entries = (globalThis.entries = {
   0: { name: "Jeasx", link: "https://www.jeasx.dev" },
   1: { name: "jsx-async-runtime", link: "https://github.com/jeasx/jsx-async-runtime" },
@@ -5,10 +35,7 @@ const entries = (globalThis.entries = {
   3: { name: "esbuild", link: "https://esbuild.github.io" },
 });
 
-/**
- * @param {import("../types").RouteProps} props
- */
-export default function ({ request }) {
+function Fragment({ request }) {
   const body = request?.body || {};
 
   if (["save", "create"].includes(body["action"])) {
@@ -23,7 +50,7 @@ export default function ({ request }) {
       {Object.entries(entries).map(([id, { name, link }]) => {
         const isEditMode = body["id"] === id && ["edit", "create"].includes(body["action"]);
         return (
-          <form class="row" fx-action="./entries" fx-method="post" fx-target="#entries">
+          <form class="row" fx-action="." fx-method="post" fx-target="#entries">
             <input type="hidden" name="id" value={id} />
             <div class="col-md-4 col-sm-12">
               {isEditMode ? (
@@ -53,7 +80,7 @@ export default function ({ request }) {
           </form>
         );
       })}
-      <form class="row" fx-action="./entries" fx-method="post" fx-target="#entries">
+      <form class="row" fx-action="." fx-method="post" fx-target="#entries">
         <div class="offset-md-10 col-md-2 col-sm-12">
           <input type="hidden" name="id" value={Object.keys(entries).length} />
           <button type="submit" name="action" value="create">
