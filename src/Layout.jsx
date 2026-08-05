0b481b1fc8f3c71d425305b2dd91ce9c59a19910
Maik Jablonski <mail@mjablonski.de>
2026-08-05 12:36:18 +0200

    upstream sync

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -78,10 +78,10 @@ export default function Layout({
   );
 }
 
-const BUILD_ID = process.env.BUILD_ID;
-
 /** @param {string} path  */
 function addCacheBuster(path, seperator = "~") {
   const index = path.lastIndexOf(".");
-  return index !== -1 ? path.slice(0, index) + seperator + BUILD_ID + path.slice(index) : path;
+  return index !== -1
+    ? path.slice(0, index) + seperator + process.env.BUILD_ID + path.slice(index)
+    : path;
 }
